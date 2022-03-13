d76c2ad055cc090d3dddcad221a5e9203a339bbc
GalacticLemonade <aidenmoore1820@gmail.com>
2022-03-12 19:34:14 -0700

    Handle hiscor

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -12,13 +12,12 @@ const spaceCode = 32;
 
 var stopwatch = document.getElementById('stopwatch-display');
 var new_best_alert = document.getElementById('new-best-alert');
+var hitime = document.getElementById('hi-time');
 var timeoutId = null;
 var ms = 0;
 var sec = 0;
 var min = 0;
 var isStarted = false
-
-console.log(store.get("WorstTime"));
 
 function start() {
     timeoutId = setTimeout(function() {
@@ -65,6 +64,38 @@ function stop() {
     let TimeScore = stopwatch.innerHTML
     clearTimeout(timeoutId);
     isStarted = false;
+    if (store.get("Best-Time")) {
+        let previousBest = store.get("Best-Time");
+        let currentBest = stopwatch.innerHTML;
+
+        let MinAndSecPrevious = previousBest.split(":");
+        let SecAndMiliPrevious = previousBest.split(".");
+
+        let MinAndSecCurrent = currentBest.split(":");
+        let SecAndMiliCurrent = previousBest.split(".");
+
+        let prevMin = MinAndSecPrevious[1];
+        let prevSec = MinAndSecPrevious[2];
+        let prevMili = SecAndMiliPrevious[2];
+
+        let curMin = MinAndSecCurrent[1];
+        let curSec = MinAndSecCurrent[2];
+        let curMili = SecAndMiliCurrent[2];
+
+        if (curMin <= prevMin) {
+            if (curSec <= prevSec) {
+                if (curMili < prevMili) {
+                    // yay!!! you got faster!!
+                    new_best_alert.innerText = "New best!";
+                    store.set("Best-Time", currentBest)
+                }
+            }
+        }
+    }else {
+        store.set("Best-Time", "30:00.00");
+        stop();
+    }
+    hitime.innerHTML = "Your current fastest solve time is: " + store.get("Best-Time") + "!"
 }
  
 function reset() {
@@ -74,6 +105,7 @@ function reset() {
     clearTimeout(timeoutId);
     stopwatch.innerHTML = '00:00.00';
     isStarted = false;
+    new_best_alert.innerText = "";
 }
 
 window.onkeydown = function(ev) {
